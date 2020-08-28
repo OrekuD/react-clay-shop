4c538b012f6fec07c8b171e77817bbfd901e5e59
unknown <opokudavid446@gmail.com>
2020-08-28 12:14:02 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return <h1> Hmm </h1>;
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header className="header"></header>;
+};
+
+export default Header;
