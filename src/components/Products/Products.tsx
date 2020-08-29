10297959655e51c6387481bdeade061d424907f9
unknown <opokudavid446@gmail.com>
2020-08-29 23:46:59 +0100

    :wrench:

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,29 +1,27 @@
 import React from "react";
 import "./Products.scss";
 import { products } from "../../data/products";
-import { purple, pink } from "../../constants/Colors";
 
 const Products = () => (
   <div className="products">
-    {products.map(({ image, name, price, description }, index) => (
-      <div className="product" key={index}>
-        <img src={image} alt={name} />
-        <div className="content">
-          <p className="card-title" style={{ color: purple }}>
-            {name}
-          </p>
-          <p className="" style={{ color: purple }}>
-            {description}
-          </p>
-          <div className="bottom-section">
-            <p className="card-title" style={{ color: purple }}>
-              ${price}
-            </p>
-            <button>Show Now</button>
+    <p className="card-title">You may like</p>
+    <div className="items">
+      {products.map(({ image, name, price, description }, index) => (
+        <div className="product" key={index}>
+          <img src={image} alt={name} />
+          <div className="content">
+            <div>
+              <p className="card-title">{name}</p>
+              <p className="description">{description}</p>
+            </div>
+            <div className="bottom-section">
+              <p className="card-title">${price}</p>
+              <button>Show Now</button>
+            </div>
           </div>
         </div>
-      </div>
-    ))}
+      ))}
+    </div>
   </div>
 );
 
