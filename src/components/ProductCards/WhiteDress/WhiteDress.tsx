4df1c92f2765f5348b327ff13f9e04e1d8dd1ab8
unknown <opokudavid446@gmail.com>
2020-08-31 12:54:58 +0100

    Add white jacket card

--- src/components/ProductCards/WhiteDress/WhiteDress.tsx
+++ src/components/ProductCards/WhiteDress/WhiteDress.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 import "./WhiteDress.scss";
+import Image from "../../../assets/images/white-dress.png";
 
-const WhiteDress = () => <div className="white-dress"></div>;
+const WhiteDress = () => (
+  <div className="white-dress">
+    <img src={Image} alt="White Dress" />
+    <div className="badge">
+      <p>-10%</p>
+    </div>
+    <div className="content">
+      <p className="card-title">Women's</p>
+      <p className="card-title">white jacket</p>
+
+      <div className="row">
+        <p className="card-title">$120</p>
+        <button className="button-main">Show Now</button>
+      </div>
+    </div>
+  </div>
+);
 
 export default WhiteDress;
